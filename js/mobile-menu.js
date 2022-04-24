a1e45cfd93c4f81c593bf51ce4671da03d661a96
Dante Kardinal <dantekardinal@gmail.com>
2022-04-24 16:38:06 +0300

    Adaptive for mobile 'header'

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,6 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button");
-    const mobileMenuRef = document.querySelector("data-menu");
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
 
     menuBtnRef.addEventListener("click", () => {
         const expanded =
@@ -8,6 +8,7 @@
         
         menuBtnRef.classList.toggle("is-open");
         menuBtnRef.setAttribute("aria-expended", !expanded);
+
         mobileMenuRef.classList.toggle("is-open");
     });
 })();
